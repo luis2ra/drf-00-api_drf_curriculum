cc8af9efd56ad521bd84fd42d6d2ae09b5dd880c
Luis Altuve <luis2ra@gmail.com>
2020-08-16 01:30:06 -0400

    feat: desarrollo del link 6

--- api_drf_curriculum/api_drf_curriculum/urls.py
+++ api_drf_curriculum/api_drf_curriculum/urls.py
@@ -5,9 +5,18 @@ from django.urls import include, path
 from django.conf.urls.static import static
 from django.contrib import admin
 
+# Login with JWT
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+)
+
 urlpatterns = [
     # Django Admin
     path('admin/', admin.site.urls),
+
+    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 
     path('', include(('users.urls', 'users'), namespace='users')),
     path('', include(('experiences.urls', 'experience'), namespace='experience')),
